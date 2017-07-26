af09513fbc6101ed10035ba51ef04a992e129dec
n0228145 <Mutual19>
2017-07-26 10:08:09 -0400

    add updates

--- Teena/StudentApplication/src/main/java/com/java/student/SpringStudentApplication.java
+++ Teena/StudentApplication/src/main/java/com/java/student/SpringStudentApplication.java
@@ -2,12 +2,16 @@ package com.java.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.java.student.model")
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrix
 public class SpringStudentApplication {
 
 	public static void main(String[] args) {
--- Teena/StudentApplication/src/main/java/com/java/student/controller/StudentController.java
+++ Teena/StudentApplication/src/main/java/com/java/student/controller/StudentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.java.student.model.Student;
 import com.java.student.model.StudentRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 
 @RestController
@@ -22,6 +23,7 @@ public class StudentController {
 	StudentRepository repository;
 	
 	@RequestMapping(value = "/add", method = RequestMethod.PUT)
+	@HystrixCommand(fallbackMethod = "displayStudents")	
 	public String add(@RequestBody Student student) {
 
 		repository.save(student);
@@ -30,6 +32,7 @@ public class StudentController {
 	}
 
 	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "displayStudents")
 	public List<Student> getStudent() {
 
 		return repository.findAll();
@@ -37,32 +40,36 @@ public class StudentController {
 	}
 
 	@RequestMapping(value = "/getStudenByName", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "displayStudents")
 	public List<Student> getStudentByName(@RequestParam("name") String studentName) {
 
 		return repository.findByName(studentName);
 	}
 	
 	@RequestMapping(value = "/getStudenById", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "displayStudents")
 	public List<Student> getStudentById(@RequestParam("id") String studentId) {
 
 		return repository.findByName(studentId);
 	}
 
 	@RequestMapping(value = "/getStudentBySubject", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "displayStudents")
 	public List<Student> getStudentBySubject(@RequestParam("subject") String subject) {
 		return repository.findBySubject(subject);
 
 	}
 	
 	@RequestMapping(value = "/updateStudent", method = RequestMethod.POST)
+	@HystrixCommand(fallbackMethod = "displayStudents")
 	public String updateStudent(@RequestBody Student student) {
 
 		if (repository.findById(student.getId()) != null) {
 			repository.save(student);
-			return "Student info record updated successfully";
+			return "Student updated successfully";
 		}
 
-		return "Student info record not found for the given Id";
+		return "Student record not found.";
 	}
 
 	@RequestMapping(value = "/deleteStudentById", method = RequestMethod.DELETE)
@@ -72,30 +79,14 @@ public class StudentController {
 			return "Student info deleted successfully for id:" + studentId;
 		}
 
-		return "Student info record not found for the given Id";
+		return "Student record not found.";
 
 	}
 
 
 
-	public List<Student> defaultStudents() {
+	public List<Student> displayStudents() {
 		return new ArrayList<Student>();
-	}
-
-	public String defaultStudents(int id) {
-		return "";
-	}
-
-	public List<Student> defaultStudents(String name) {
-		return new ArrayList<Student>();
-	}
-
-	public String defaultStudents(Student student) {
-		return "";
-	}
-
-	public String defaultDeleteStudents(String string) {
-		return "";
 	}
 
 }
